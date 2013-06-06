40e9dbb7c4903a3b322d55e508e828621e840572
Tom Buchok <git@buchok.com>
2013-06-06 16:38:34 -0400

    adds while loop to keep server running

--- server.c
+++ server.c
@@ -20,16 +20,21 @@ int main(void) {
   server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 
   server_fd = socket(PF_INET, SOCK_STREAM, 0);
-  printf("Socket connected FD %i ... \n", server_fd);
+  printf("| Socket connected FD %i ... \n", server_fd);
   int bound = bind(server_fd, (struct sockaddr*)&server_address, sizeof (struct sockaddr));
-  printf("Bound ... %i\n", bound);
+  printf("| Bound ... %i\n", bound); // Log
   int listening = listen(server_fd, 10);
-  printf("Listening ... %i\n", listening);
-  client_fd = accept(server_fd, (struct sockaddr*)&server_address, &server_address_size);
-  printf("Socket connected FD %i ... \n", client_fd);
-  char msg[] = "Yay! You did it!\n";
-  send(client_fd, &msg, sizeof msg, 0);
-  close(client_fd);
+  printf("| Listening ... %i\n", listening); // Log
+  while (1) {
+    printf("Accepting a connections ...\n");
+    client_fd = accept(server_fd, (struct sockaddr*)&server_address, &server_address_size);
+    printf("> Received connection %i ... \n", client_fd);
+    char msg[1024];
+    recv(client_fd, &msg, 1024, 0);
+    printf("< Sending :%s", msg); // Log
+    send(client_fd, &msg, sizeof msg, 0);
+    close(client_fd);
+  }
   close(server_fd);
   printf("Closing down the server ... \n");
   return 0; // end!
